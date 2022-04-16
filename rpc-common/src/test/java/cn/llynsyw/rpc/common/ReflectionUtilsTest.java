f16faf5c063aa3dcd16c85039b867d40f4d02a86
lgc <2939613064@qq.com>
2022-04-16 22:28:10 +0800

    Modify:Modify on rpc-commit/src/test/java/ReflectionUtilsTest

--- rpc-common/src/test/java/cn/llynsyw/rpc/common/ReflectionUtilsTest.java
+++ rpc-common/src/test/java/cn/llynsyw/rpc/common/ReflectionUtilsTest.java
@@ -1,5 +1,6 @@
 package cn.llynsyw.rpc.common;
 
+import cn.llynsyw.rpc.proto.ServiceDescriptor;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,13 +13,15 @@ import static org.junit.Assert.*;
  * @author lgc
  */
 public class ReflectionUtilsTest {
-    ProtoUtils pro;
-    Method[] met1;
+    RegistryUtils reg;//提供方法的实体
+    Method[] met1;//提供参数方法
+    Method[] met2;//提供方法
     @Before
     public void setUp()  {
         //每个test方法之前执行，可以在这里创建需要的对象
-        pro = new ProtoUtils();
+        reg = new RegistryUtils();
         met1 = ReflectionUtils.getPublicMethods(ProtoUtils.class);
+        met2 = ReflectionUtils.getPublicMethods(RegistryUtils.class);
     }
 
     @After
@@ -28,16 +31,15 @@ public class ReflectionUtilsTest {
     }
 
     @Test
-    public void newInstance() {
+    public void testNewInstance() {
         //测试能否通过Class对象生成实例
         ProtoUtils pro2 = ReflectionUtils.newInstance(ProtoUtils.class);
         assertEquals(ProtoUtils.class,pro2.getClass());
     }
     @Test
-    public void invoke() {
-        //第五个方法为对象转字符数组，这里比较返回的对象
-        Object bytes = ReflectionUtils.invoke(pro,met1[5],"String对象");
-        byte[] bb = new byte[]{0};
-        assertEquals(bb.getClass(),bytes.getClass());
+    public void testInvoke() {
+        //RegistryUtils的方法
+        Object ser = ReflectionUtils.invoke(reg,met2[0],ProtoUtils.class,met1[0]);
+        assertEquals(ServiceDescriptor.class,ser.getClass());
     }
 }
